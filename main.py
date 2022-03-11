d7448838e5fdad4cd0a192b5dca9128a4ea8429c
Sancaraballo <scaraballo@teco.com.ar>
2022-03-11 12:12:22 -0300

    main.py: defining setter and getter

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from curses.ascii import isalpha
+
+
 class Backpack:
 
     max_num_items = 10
     
     def __init__(self, color, tamaño):
-        self.items = []
+        self._items = []
         self.color = color
         self.size = tamaño
+
+    def get_items(self):
+        return self._items
+    
+    def set_items(self, item):
+        if isinstance(item, str) and isalpha(item):
+            self._items.append(item)
+        else:
+            print("That's not a real item")
 
 my_bag = Backpack('red', 14)
 my_bag.items.append('pencil')
