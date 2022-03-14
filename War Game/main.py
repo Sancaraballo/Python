3d54106c6c3141780f10e695cffa65748f63b4da
Sancaraballo <scaraballo@teco.com.ar>
2022-03-14 17:55:18 -0300

    refactoring code to modular. Adding Suit.py

--- War Game/main.py
+++ War Game/main.py
@@ -1,21 +1,5 @@
 # War game Deck: 52 cards, Suits: 'Clubs', 'Diamond', 'Hearts' or 'Spades'
 
-class Suit:
-
-    SYMBOLS = {"clubs":"♣", "diamonds":"♦", "hearts":"♥", "spades":"♠"}
-
-    def __init__(self, description):
-        self._description = description
-        self._symbol = Suit.SYMBOLS[description]
-
-    @property
-    def description(self):
-        return self._description
-    
-    @property
-    def symbol(self):
-        return self._symbol
-
 class Card:
     suit = Suit()
     
