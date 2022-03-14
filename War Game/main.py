699a988b09879eab4baf96c0d31e3cd00c7db693
Sancaraballo <scaraballo@teco.com.ar>
2022-03-14 17:49:16 -0300

    main.py: create class Suit

--- War Game/main.py
+++ War Game/main.py
@@ -1,11 +1,20 @@
 # War game Deck: 52 cards, Suits: 'Clubs', 'Diamond', 'Hearts' or 'Spades'
 
-
 class Suit:
-    description = ["clubs", "diamonds", "hearts", "spades"]
-    symbol = ["♣", "♦", "♥", "♠"]
 
+    SYMBOLS = {"clubs":"♣", "diamonds":"♦", "hearts":"♥", "spades":"♠"}
 
+    def __init__(self, description):
+        self._description = description
+        self._symbol = Suit.SYMBOLS[description]
+
+    @property
+    def description(self):
+        return self._description
+    
+    @property
+    def symbol(self):
+        return self._symbol
 
 class Card:
     suit = Suit()
@@ -19,4 +28,19 @@ class Card:
         return self.value in [11, 12, 13, 14]
 
 class Deck:
-    _cards = Card()
+    _cards = []
+
+    def build(self):
+        for i in range(52):
+            i = Card()
+            if i not in self._cards:
+                self._cards.append(Card())
+    
+    def shuffle(self):
+        pass
+
+    def show(self):
+        pass
+    
+    def draw(self):
+        pass
